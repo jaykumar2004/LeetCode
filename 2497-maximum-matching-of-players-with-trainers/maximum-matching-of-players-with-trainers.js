011838b8442de03d8b39bc51571f405c9f07d9e6
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-06 16:24:32 +0530

    Time: 100 ms (16.67%) | Memory: 67.9 MB (59.26%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} players
+ * @param {number[]} trainers
+ * @return {number}
+ */
+var matchPlayersAndTrainers = function(g, s) {
+    g.sort((a, b) => (a - b));
+    s.sort((a, b) => (a - b));
+
+    let i = 0;
+    let j = 0;
+
+    while (j < s.length) {
+        if (s[j] >= g[i]) {
+            ++i;
+        }
+        ++j
+    }
+    return i;
+};
