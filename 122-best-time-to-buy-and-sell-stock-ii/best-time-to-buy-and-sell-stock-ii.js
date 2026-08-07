f657cf37e44bd3f6653942723c918c46ee593d53
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-07 18:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (33.51%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -5,7 +5,7 @@
 var maxProfit = function (prices) {
     let ans = 0;
 
-    for (let i = 1; i < prices.length; i++) {
+    for (let i = 0; i < prices.length; i++) {
         let profit = prices[i] - prices[i - 1];
         if (profit > 0) {
             ans += profit;
